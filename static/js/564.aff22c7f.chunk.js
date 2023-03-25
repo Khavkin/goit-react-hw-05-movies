d68f7b39ac5803b6f60a120712670f8e0eadcfdb
Khavkin <hdm@ukr.net>
2023-03-25 15:43:08 +0000

    Deploying to gh-pages from @ Khavkin/goit-react-hw-05-movies@b220ee498c23f7adc982a754ead20e0c8539345a 🚀

---
+++ static/js/564.aff22c7f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[564],{564:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(184),u=function(){return(0,r.jsx)("div",{children:"Page not found"})}}}]);
+//# sourceMappingURL=564.aff22c7f.chunk.js.map
---
+++ static/js/98.6461b298.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[98],{98:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(184),u=function(){return(0,n.jsx)("div",{children:"It's review"})}}}]);
+//# sourceMappingURL=98.6461b298.chunk.js.map
